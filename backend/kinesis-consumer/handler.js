2ce382733a8a2bb01ed8eaedde26dee008b2cd0e
ebahsini <evan@tytsky.com>
2017-03-18 19:29:45 -0400

    adds basic kinesis consumer service

---
+++ backend/kinesis-consumer/handler.js
@@ -0,0 +1,65 @@
+"use strict";
+
+// handler configuration
+const config = {};
+config.REDIS_HOST = process.env.REDIS_HOST;
+config.REDIS_PORT = process.env.REDIS_PORT;
+config.REGION = process.env.REGION;
+config.STAGE = process.env.STAGE;
+
+// aws configuration
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: config.REGION
+});
+var Redis = require('ioredis');
+var redis = null;
+
+
+// main event handler
+module.exports.consumeKinesis = (event, context, callback) => {
+  //console.log(JSON.stringify(event, null, 4));
+  // manual hoisting
+  var SBX = {};
+  SBX.config = config;
+  SBX.events = [];
+
+  // setup redis connection
+  // add reconnectOnError option when multi-az
+ // redis = new Redis(config.REDIS_PORT, config.REDIS_HOST);
+
+  // process stream data
+  parseKinesis(event.Records, SBX);
+
+  // testing artifacts
+  //console.log(SBX.events);
+  //SBX.events = SBX.events.slice(0, 1);
+  console.log("event-zero ::", SBX.events[0]);
+  console.log("event-five ::", SBX.events[4]);
+  console.log("event-count ::", SBX.events.length);
+
+  // leave happy
+  getOut();
+};
+
+
+// helpers
+function parseKinesis(records, ledger) {
+  // avoid empty strings (dynamodb-unfriendly)
+  // obtain url-events from records
+  var events = [];
+  records.map((record) => {
+   // parse kinesis data
+    var buffer = new Buffer(record.kinesis.data, "base64");
+    var payload = JSON.parse(buffer.toString("utf8"));
+    // BUILD ARRAY of event objects
+    //console.log (payload);
+    events = events.concat(payload)
+  });
+  ledger.events = events;
+}
+
+function getOut(ledger) {
+  // get done
+ //  redis.quit();
+}
